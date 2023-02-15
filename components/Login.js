f4c2153705f4608ad591e5e539f6608ce45748ff
Gary <garyarneys@gmail.com>
2023-02-14 20:41:44 -0600

    Somehow got the token authorization to work, going to try to capture the inputs from user/password

--- components/Login.js
+++ components/Login.js
@@ -3,12 +3,40 @@ import React from 'react';
 
 const Login = () => {
 
+
+    const login = async() => {
+            try {
+              const response = await fetch('https://strangers-things.herokuapp.com/api/2211-FTB-ET-WEB-AM/users/register', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  user: {
+                    username: 'GGGG',
+                    password: 'FFFF'
+                  }
+                })
+              });
+        
+              const result = await response.json();
+              console.log(result.data.token);
+              window.localStorage.setItem('token',result.data.token);
+            } catch (error) {
+              console.error(error);
+            }
+
+          };
+          login();
+   
+
     return(
         <>
         <form>
 
             <input type="text" placeholder="username"></input>
             <input type="text" placeholder="password"></input>
+            <button onClick={login}>Login</button>
 
         </form>
         </>
